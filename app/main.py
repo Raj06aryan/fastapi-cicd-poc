b1d03232f04a932e735e2dc27c2d6a47a80b8110
Raj Aryan <rajaryan1879@gmail.com>
2026-07-07 15:41:13 +0530

    changed home route

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ r = redis.Redis(
 
 @app.get("/")
 def home():
-    return {"message": "hello world welcome to fastapi cicd pipeline hello"}
+    return {"message": "hello world welcome to fastapi cicd pipeline"}
 
 @app.get("/health")
 def health():
