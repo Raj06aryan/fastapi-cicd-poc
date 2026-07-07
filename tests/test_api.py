c74d7b82065194c6f9784cbfeeeeafd6155caf9d
Raj Aryan <rajaryan1879@gmail.com>
2026-07-07 13:52:25 +0530

    modified test file accordingly to check feature branch

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json()["message"] == "hello world welcome to fastapi cicd pipeline"
+    assert response.json()["message"] == "hello world i am changing the home message from feature branch"
 
 def test_health():
     response = client.get("/health")
